55989f2e425269e31956885d2be057a284be014b
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-02-02 22:53:57 +0100

    adding learning js file

---
+++ learning/index.js
@@ -0,0 +1,40 @@
+// Logging to the console
+
+
+// Variables
+
+
+// Primitive data types --> Strings, Numbers, Booleans
+
+
+// Control flow
+
+
+// Loops
+
+
+// Functions
+
+
+// Scope
+
+
+// Objects
+
+
+// Arrays
+
+
+// Builtins
+
+
+// Context & this
+
+
+// DOM
+
+
+// Event listeners
+
+
+// Event delegation
