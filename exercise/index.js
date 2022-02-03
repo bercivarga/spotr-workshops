05a909492ec5cd633f5877496687c4124ac7624a
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-02-03 15:15:57 +0100

    add exercise base js

--- exercise/index.js
+++ exercise/index.js
@@ -1,8 +1,22 @@
 // Calculator project
+const display = document.querySelector('.display');
+const plusBtn = document.querySelector('.add');
 
-/*
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
 
-	Button classnames: display, divide, multiply, substract, add, clear, delete, equal, one, two, ...
+const clear = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
 
-*/
+const numButtons = [ one, two, three, four, five, six, seven, eight, nine ];
 
+const total = 0;
+const displayed = "0";
+const lastHit;
