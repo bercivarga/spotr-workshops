77eebe3aa676259820409b3145c19ad785d73e9f
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-02-09 15:50:01 +0100

    add exercises

---
+++ exercises/fetch.js
@@ -0,0 +1,14 @@
+// const API_URL = "";
+
+// const promise = fetch(API_URL);
+
+// promise
+//   .then(function(response) {
+//     const processingPromise = response.json();
+//     return processingPromise;
+//   })
+//   .then(function(processedResponse) {
+//     console.log(processedResponse);
+//   });
+
+// console.log("this will log first");
