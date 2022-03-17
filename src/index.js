09a7ebcca55dcfda166c59f5903d11acb5139392
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-03-17 09:13:13 +0100

    add base info

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Strict mode
+
+// Scope
+
+// Hoisting
+
+// Var, let, const
+
+// Types and coercion
+
+// Inheritance
+
+// DOM vs Shadow DOM vs Virtual DOM
+
+// Linters and formatters
+
+// Build tools
+
+// Frameworks
+
+// What's to come? -- React, TypeScript, Modern CSS and frameworks, and lots of fun!!
